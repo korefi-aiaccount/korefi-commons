78627bd8609bde504e9ce6a673bdc81080afa67f
Bitbucket Pipeline <noreply@bitbucket.org>
2025-01-23 06:32:47 +0000

    Update version in setup.py to v0.2.1 [skip ci]

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="korefi-commons",
-    version="0.2.0",
+    version="0.2.1",
     packages=find_packages(),
     install_requires=[
         "boto3",
